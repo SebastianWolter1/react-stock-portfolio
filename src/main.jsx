255dfb39dc69b2b6a05c00d781d3fd91809cca0c
SebastianWolter1 <seb@techtakel.berlin>
2023-01-17 10:16:03 +0100

    finalcommit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
 import StockContextProvider from "./global/Context";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
   <StockContextProvider>
     <App/>
   </StockContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
